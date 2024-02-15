7b6440c98047cd78f43b48bf12f3a943982cb685
muhammadnasr <muhammad.hassan.nasr@gmail.com>
2024-02-15 11:46:51 +0300

    adds some text to test chromatic build

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Welcome = () => {
   return (
-    <p>Hi, welcome to Kittl's full stack assignment for collaboration team!</p>
+    <p>
+      Hi, welcome to Kittl's full stack assignment for collaboration team!
+      <br/>
+      Solution by Muhammad Nasr...
+    </p>
   )
 };
 
